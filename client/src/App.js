aa3c15d4a5b77ba5e36f73a3332fc531e9558701
Radoslav Stefanov <radoslav.g.stefanov@gmail.com>
2023-03-05 18:37:56 +0200

    Sepated the components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Loading from "./components/Loading";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Todos from "./components/Todos";
 
-function App() {
+function App() 
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Header/>
+
+      <main className="main">
+        <section className="todo-list-container">
+          <h1>Todo List</h1>
+
+          <div className="add-btn-container">
+            <button className="btn">+ Add new Todo</button>
+          </div>
+
+          <div className="table-wrapper">
+
+            {/* <Loading/> */}
+            <Todos/>
+            
+          </div>
+        </section>
+      </main>
+
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/components/Todos.js
@@ -0,0 +1,25 @@
+export default function Todos()
+{
+    return(
+        <table className="table">
+              <thead>
+                <tr>
+                  <th className="table-header-task">Task</th>
+                  <th className="table-header-status">Status</th>
+                  <th className="table-header-action">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                <tr className="todo is-completed">
+                  <td>Give dog a bath</td>
+                  <td>Complete</td>
+                  <td className="todo-action">
+                    <button className="btn todo-btn">Change status</button>
+                  </td>
+                </tr>
+                
+              </tbody>
+            </table>
+    );
+}
